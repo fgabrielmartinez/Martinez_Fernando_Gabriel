cdf9fe64c6d2814cea08a9c4ccf474512a57be91
fgabrielmartinez <112987966+fgabrielmartinez@users.noreply.github.com>
2022-09-07 19:19:53 -0300

    carga de ejercicios

---
+++ Ejercicio6.js
@@ -0,0 +1,2 @@
+var dias = ["Lunes","Martes","Miercoles","Jueves","Viernes","Sábado","Domingo"];
+console.log (dias[0],dias[1],dias[2],dias[3],dias[4],dias[5],dias[6]);
--- tp.js
+++
@@ -1,2 +0,0 @@
-
-// ahora estoy cambiando cosas
